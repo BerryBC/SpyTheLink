fa1b10948880b510e3d0ac6afa8e0fdd6922ca62
BerryBC <Berry_BC_@163.com>
2020-02-09 00:19:45 +0800

    在新增路径的时候清洗一下路径

--- Lib/LAddPage/__init__.py
+++ Lib/LAddPage/__init__.py
@@ -3,7 +3,7 @@
 @Author: BerryBC
 @Date: 2020-02-02 14:24:17
 @LastEditors  : BerryBC
-@LastEditTime : 2020-02-04 16:18:39
+@LastEditTime : 2020-02-09 00:19:02
 '''
 import time
 
@@ -23,11 +23,12 @@ class claAddPage(object):
         # print(strHref)
         if not strHref is None:
             bolHttps = ('http://' in strHref or 'https://' in strHref)
+            strCleanURL=self.CleanURL(strHref)
             if bolHttps:
-                if not self.objMongoDB.CheckOneExisit('pagedb-Crawled', {'url': strHref}):
+                if not self.objMongoDB.CheckOneExisit('pagedb-Crawled', {'url': strCleanURL}):
                     dictNewPage = self.AnEmptyPageEle()
-                    intDepth = len(strHref.split('/'))-3
-                    dictNewPage['url'] = strHref
+                    intDepth = len(strCleanURL.split('/'))-3
+                    dictNewPage['url'] = strCleanURL
                     dictNewPage['d'] = intDepth
                     self.objMongoDB.InsertOne('pagedb-Crawled', dictNewPage)
 
@@ -37,9 +38,19 @@ class claAddPage(object):
         for eleP in arrTagP:
             strPContent += eleP.get_text()+' '
         dictNewContent = self.AnEmptyContentEle()
-        dictNewContent['ct']=strPContent
-        if len(strPContent)>20:
+        dictNewContent['ct'] = strPContent
+        if len(strPContent) > 20:
             # print('   成功爬了一个网站')
             self.objMongoDB.InsertOne('sampledb', dictNewContent)
         # else :
         #     print('   字数不够不保存')
+
+    def CleanURL(self, strURL):
+        strRealURL = strURL
+        intBQ = strURL.find('?')
+        if intBQ > 0:
+            strRealURL = strURL[:intBQ]
+        intBQ = strRealURL.find('#')
+        if intBQ > 0:
+            strRealURL = strRealURL[:intBQ]
+        return strRealURL
