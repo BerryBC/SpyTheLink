17ecac65e6565eaab2bd5f64309b26b819509f72
BerryBC <Berry_BC_@163.com>
2020-04-28 23:58:27 +0800

    更新上去初始化信息

--- Lib/LLearn/__init__.py
+++ Lib/LLearn/__init__.py
@@ -3,7 +3,7 @@
 @Author: BerryBC
 @Date: 2020-04-27 22:29:02
 @LastEditors: BerryBC
-@LastEditTime: 2020-04-28 23:39:38
+@LastEditTime: 2020-04-28 23:58:15
 '''
 import joblib
 import jieba
@@ -21,6 +21,7 @@ class claLearn(object):
 
     def LoadLatestClf(self):
         self.objLatestClfCfg=self.objMongoDB.LoadOneBySort('clfdb', {},[('lt',-1)])
+        self.clfLatestClf=BaggingClassifier(base_estimator=LinearSVC(random_state=0, tol=1e-05, max_iter=10000))
         self.clfLatestClf= joblib.load("../SpyDataWebAppAndAPI/ClfFile/"+self.objLatestClfCfg['clfFileName'])
 
     def JudContent(self,arrP):
